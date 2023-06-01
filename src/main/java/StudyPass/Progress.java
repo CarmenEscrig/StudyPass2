3b8bb54d4d0f3a28de8c9bd1fcd13b635020f166
CarmenEscrig <carmenescrig.yijiao@gmail.com>
2023-06-01 09:59:17 +0200

    Comit porque a Hector le apetece

---
+++ src/main/java/StudyPass/Progress.java
@@ -0,0 +1,43 @@
+package StudyPass;
+
+public class Progress {
+    private int id;
+    private int correct;
+    private int incorrect;
+
+    public Progress() {
+        this.id = -1;
+        this.correct = 0;
+        this.incorrect = 0;
+    }
+
+    public Progress(int id, int correct, int incorrect) {
+        this.id = id;
+        this.correct = correct;
+        this.incorrect = incorrect;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCorrect() {
+        return correct;
+    }
+
+    public void setCorrect(int correct) {
+        this.correct = correct;
+    }
+
+    public int getIncorrect() {
+        return incorrect;
+    }
+
+    public void setIncorrect(int incorrect) {
+        this.incorrect = incorrect;
+    }
+}
