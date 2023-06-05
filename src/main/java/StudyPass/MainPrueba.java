2884cf6b8d07e71baa38b985ee1112a266eb378d
CarmenEscrig <carmenescrig.yijiao@gmail.com>
2023-06-05 10:26:13 +0200

    todo?

--- src/main/java/StudyPass/MainPrueba.java
+++ src/main/java/StudyPass/MainPrueba.java
@@ -6,29 +6,17 @@ import java.sql.SQLException;
 public class MainPrueba {
     static Connection con = DatabaseConnection.getInstance().getConnection();
     public static void main(String[] args) throws SQLException {
-        /*User user = new User("Pepe", "Taller2014", "student");
-        UserRepositoryImpl userRepository = new UserRepositoryImpl();
-        userRepository.save(user);
-        for (User user1 : userRepository.findAll()) {
-            System.out.println(user1.getUsername());
-        }
-        userRepository.delete(user);
-*/
-        Subject subject = new Subject("Matematicas");
+        Subject subject = new Subject("matematicas");
+        Subject subject1 = new Subject("fisica");
         SubjectRepositoryImpl subjectRepository = new SubjectRepositoryImpl();
         subjectRepository.save(subject);
-        subjectRepository.findAll();
-
-        FlashCard flashCard = new FlashCard("algo","si",subject);
-        FlashRepositoryImpl flashRepository = new FlashRepositoryImpl();
-        flashRepository.save(flashCard);
-        flashRepository.findAll();
-
-        Progress progress = new Progress(12,15);
-        ProgressRepositoryImpl progressRepository = new ProgressRepositoryImpl();
-        progressRepository.save(progress);
-        progressRepository.findAll();
-
-
+        subjectRepository.save(subject1);
+        User user = new User("Pepe", "Taller2014", "student");
+        user.addSubject(subject1);
+        user.addSubject(subject);
+        subject1.addUser(user);
+        subject.addUser(user);
+        UserRepositoryImpl userRepository = new UserRepositoryImpl();
+        userRepository.save(user);
     }
 }
